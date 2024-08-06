24e16b2df33e07f52e062f5efd7d3daafc0eee0a
AMUTHAN S <138638866+AMUTHAN21@users.noreply.github.com>
2024-08-06 21:39:31 +0530

    Time: 11 ms (46.54%) | Memory: 45.9 MB (17.31%) - LeetSync

--- 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
+++ 3276-minimum-number-of-pushes-to-type-word-ii/minimum-number-of-pushes-to-type-word-ii.java
@@ -1,18 +1,22 @@
-class Solution 
-{
-    public int minimumPushes(String word) 
-    {
-        int arr[] = new int[26];
-		for (char ch : word.toCharArray()) 
-        {
-			arr[ch - 'a']--;
-		}
-        Arrays.sort(arr);
-        int count=0;
-		for (int i = 0; i < 26; i++) 
-        {
-			count -= arr[i] * (i / 8 + 1);
-		}
-		return count;
+class Solution {
+    public int minimumPushes(String word) {
+        int[] letterFrequency = new int[26];
+        for (char c : word.toCharArray()) {
+            letterFrequency[c - 'a']++;
+        }
+        
+        Integer[] sortedFreq = new Integer[26];
+        for (int i = 0; i < 26; i++) {
+            sortedFreq[i] = letterFrequency[i];
+        }
+        Arrays.sort(sortedFreq, Collections.reverseOrder());
+        
+        int totalPresses = 0;
+        for (int i = 0; i < 26; i++) {
+            if (sortedFreq[i] == 0) break;
+            totalPresses += (i / 8 + 1) * sortedFreq[i];
+        }
+        
+        return totalPresses;
     }
 }
